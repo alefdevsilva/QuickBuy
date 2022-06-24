e689917dbfd8260d45f750c23022cbb710e8360c
Alef Silva <alef.dev.silva@gmail.com>
2022-06-23 22:40:00 -0300

    Criacao das classes de tipo enumerado e objeto de valor

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using QuickBuy.Dominio.ObjetoDeValor;
 
 namespace QuickBuy.Dominio.Entidades
 {
@@ -8,8 +9,16 @@ namespace QuickBuy.Dominio.Entidades
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
         public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string Cep { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public string NumeroEndereco { get; set; }
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
         /// <summary>
-        /// Pedidos deve ter um ou muitos pedidos
+        /// Pedidos deve ter pelo menos um item de pedido ou muitos itens pedidos
         /// </summary>
         public ICollection<ItemPedido> ItemPedido { get; set; }
     }
