fd3e4faa04a180b5236359dac9308b59b702da31
Alef Silva <alef.dev.silva@gmail.com>
2022-06-23 22:12:28 -0300

    Definindo entidades

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        /// <summary>
+        /// Pedidos deve ter um ou muitos pedidos
+        /// </summary>
+        public ICollection<ItemPedido> ItemPedido { get; set; }
+    }
+}
