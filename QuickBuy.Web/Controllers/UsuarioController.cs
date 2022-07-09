c5b91f22291f895b9a090d87beb70b813beb239f
Alef Silva <alef.dev.silva@gmail.com>
2022-07-09 13:31:54 -0300

    Injetando UsuarioRepository no UsuarioController

---
+++ QuickBuy.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using QuickBuy.Dominio.Contratos;
+using QuickBuy.Dominio.Entidades;
+
+namespace QuickBuy.Web.Controllers
+{
+    [Route("api/[Controller]")]
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+        public UsuarioController(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        [HttpPost("VerificarUsuario")]
+        public ActionResult VerificarUsuario([FromBody]Usuario usuario)
+        {
+            try
+            {
+                var usuarioRetorno = _usuarioRepositorio.ObterPorId(usuario.Id);
+                if (usuarioRetorno != null)
+                    return Ok(usuarioRetorno);
+
+                return BadRequest("Usuario e senha inválido!");
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
